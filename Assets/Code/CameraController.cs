8bd46fefce339723b5c8393a8ac539b67af803f3
Corey Urquhart <CoreyU49@gmail.com>
2017-07-20 18:21:05 +0100

    Tidy Up
    - Sorted files into appropriate folders.
    - Updated camera controller to feature smoother movement.
    - Changed filter type for temp graphics to point; I believe this was the desired effect.
    - Added worldspace canvases for nodes/chips so the text looks smoother.
    - Commented out some code in ObjectDataNode.cs as it was causing errors and lacked functionality in it's current state.

---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public int zoomLevel = 1;
+	public int maxZoomLevel = 12;
+	public int minZoomLevel = 1;
+
+	private bool isDragging = false;
+	private Vector3 dragStartPosition;
+	private Vector3 dragMousePosition;
+
+	private Vector3 camDest;
+
+	void Start() {
+
+		camDest = transform.position;
+
+	}
+
+	private void Update() {
+
+		HandlePanning();
+		HandleScrolling();
+
+		transform.position += ( camDest - transform.position ) / 15f;
+		Camera.main.orthographicSize += ( zoomLevel - Camera.main.orthographicSize ) / 20f;
+
+	}
+
+	public void HandlePanning() {
+
+		if( Input.GetMouseButton( 2 ) ) {
+
+			if( !isDragging ) {
+
+				isDragging = true;
+				dragStartPosition = transform.position;
+				dragMousePosition = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+
+			}
+
+			Vector3 panOffset = dragMousePosition - Camera.main.ScreenToWorldPoint( Input.mousePosition );
+
+			camDest += panOffset;
+			dragMousePosition = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+
+		} else {
+
+			isDragging = false;
+
+		}
+
+	}
+
+	private void HandleScrolling() {
+
+		float d = Input.GetAxis( "Mouse ScrollWheel" );
+
+		if( d < 0 ) {
+			zoomLevel++;
+			if( zoomLevel > maxZoomLevel ) {
+				zoomLevel = maxZoomLevel;
+			}
+		} else if( d > 0 ) {
+			zoomLevel--;
+			if( zoomLevel < minZoomLevel ) {
+				zoomLevel = minZoomLevel;
+			}
+
+		}
+
+	}
+}
